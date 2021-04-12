c47454d3178c81068cdf29feb6477aa6dad09da3
Gustavo Kich <gh.kich@gmail.com>
2021-04-11 21:43:05 -0300

    Refactor: Remove throw error inside fetchFormSource

--- imports/infra/fetchFromSource.js
+++ imports/infra/fetchFromSource.js
@@ -1,10 +1,7 @@
 export const fetchFromSource = (url) =>
   new Promise((resolve, reject) =>
     fetch(url)
-      .then((data) => {
-        if (!data) throw new Error('No data fetched')
-        return data.json()
-      })
+      .then((resp) => resp.json())
       .then((data) => resolve(data))
       .catch((error) => reject(error)),
   )
